896e0f0f4a89913c8cb9a6db97c31e3b68f7f703
Fritjof <fritjoflammers@gmail.com>
2023-08-02 13:30:17 +0200

    wrapper using tempdir

--- bio/bismark/bismark/wrapper.py
+++ bio/bismark/bismark/wrapper.py
@@ -7,7 +7,11 @@ __email__ = "roman.chernyatchik@jetbrains.com"
 __license__ = "MIT"
 
 import os
+import shutil
 
+import pdb 
+
+from pathlib import Path
 from snakemake.shell import shell
 from tempfile import TemporaryDirectory
 
@@ -27,11 +31,6 @@ extra = snakemake.params.get("extra", "")
 cmdline_args = ["bismark {extra} --bowtie2"]
 
 outdir = os.path.dirname(snakemake.output.bam)
-if outdir:
-    cmdline_args.append("--output_dir {outdir}")
-
-genome_indexes_dir = os.path.dirname(snakemake.input.bismark_indexes_dir)
-cmdline_args.append("{genome_indexes_dir}")
 
 if not snakemake.output.get("bam", None):
     raise ValueError("bismark/bismark: Error 'bam' output file isn't specified.")
@@ -45,58 +44,83 @@ if snakemake.params.get("basename", None):
 else:
     basename = None
 
-# reads input
-single_end_mode = snakemake.input.get("fq", None)
-if single_end_mode:
-    # for SE data, you only have to specify read1 input by -i or --in1, and
-    # specify read1 output by -o or --out1.
-    cmdline_args.append("--se {snakemake.input.fq:q}")
-    mode_prefix = "se"
-    if basename is None:
-        basename = basename_without_ext(snakemake.input.fq)
-else:
-    # for PE data, you should also specify read2 input by -I or --in2, and
-    # specify read2 output by -O or --out2.
-    cmdline_args.append("-1 {snakemake.input.fq_1:q} -2 {snakemake.input.fq_2:q}")
-    mode_prefix = "pe"
 
-    if basename is None:
-        # default basename
-        basename = basename_without_ext(snakemake.input.fq_1) + "_bismark_bt2"
 
 # log
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
-cmdline_args.append("{log}")
 
-# run
-shell(" ".join(cmdline_args))
+with TemporaryDirectory() as tempdir:
+    tempdir = Path(tempdir)
+    # run
+    genome_indexes_dir = Path(tempdir, "genome/")
+    # copy input files into tempdir
+    os.makedirs(genome_indexes_dir)
+    shutil.copy(snakemake.input.genome, genome_indexes_dir)
+    shutil.copytree(os.path.dirname(snakemake.input.bismark_indexes_dir), genome_indexes_dir, dirs_exist_ok=True)
+    for input_file in snakemake.input.fq_1, snakemake.input.fq_2, snakemake.input.genomic_freq:
+        shutil.copy(input_file, tempdir)
+               
+    cmdline_args.append("{genome_indexes_dir}")
 
-# Move outputs into proper position.
-expected_2_actual_paths = [
-    (
-        snakemake.output.bam,
-        os.path.join(
-            outdir, "{}{}.bam".format(basename, "" if single_end_mode else "_pe")
-        ),
-    ),
-    (
-        snakemake.output.report,
-        os.path.join(
-            outdir,
-            "{}_{}_report.txt".format(basename, "SE" if single_end_mode else "PE"),
-        ),
-    ),
-    (
-        snakemake.output.get("nucleotide_stats", None),
-        os.path.join(
-            outdir,
-            "{}{}.nucleotide_stats.txt".format(
-                basename, "" if single_end_mode else "_pe"
+    # reads input
+    single_end_mode = snakemake.input.get("fq", None)
+    if single_end_mode:
+        # for SE data, you only have to specify read1 input by -i or --in1, and
+        # specify read1 output by -o or --out1.
+        input_fq = os.path.join(tempdir, os.path.basename(snakemake.input.fq))
+        cmdline_args.append("--se {input_fq:q}")
+        mode_prefix = "se"
+        if basename is None:
+            basename = basename_without_ext(snakemake.input.fq)
+    else:
+        # for PE data, you should also spec ify read2 input by -I or --in2, and
+        # specify read2 output by -O or --out2.
+        input_fq1 = os.path.join(tempdir, os.path.basename(snakemake.input.fq_1))
+        input_fq2 = os.path.join(tempdir, os.path.basename(snakemake.input.fq_2))
+
+        cmdline_args.append("-1 {input_fq1:q} -2 {input_fq2:q}")
+        mode_prefix = "pe"
+
+        if basename is None:
+            # default basename
+            basename = basename_without_ext(snakemake.input.fq_1) + "_bismark_bt2"
+
+    cmdline_args.append(f"--output_dir {tempdir} ")
+
+    cmdline_args.append("{log}")
+
+    shell(" ".join(cmdline_args))
+
+
+    # Move outputs into proper position.
+    expected_2_actual_paths = [
+        (
+            snakemake.output.bam,
+            os.path.join(
+                tempdir, "{}{}.bam".format(basename, "" if single_end_mode else "_pe")
             ),
         ),
-    ),
-]
-log_append = snakemake.log_fmt_shell(stdout=True, stderr=True, append=True)
-for exp_path, actual_path in expected_2_actual_paths:
-    if exp_path and (exp_path != actual_path):
-        shell("mv {actual_path:q} {exp_path:q} {log_append}")
+        (
+            snakemake.output.report,
+            os.path.join(
+                tempdir,
+                "{}_{}_report.txt".format(basename, "SE" if single_end_mode else "PE"),
+            ),
+        ),
+        # (
+        #     snakemake.output.get("nucleotide_stats", None),
+        #     os.path.join(
+        #         tempdir,
+        #         "{}{}.nucleotide_stats.txt".format(
+        #             basename, "" if single_end_mode else "_pe"
+        #         ),
+        #     ),
+        # ),
+    ]
+
+    pdb.set_trace()
+    
+    log_append = snakemake.log_fmt_shell(stdout=True, stderr=True, append=True)
+    for exp_path, actual_path in expected_2_actual_paths:
+        if exp_path and (exp_path != actual_path):
+            shell("mv {actual_path:q} {exp_path:q} {log_append}")
