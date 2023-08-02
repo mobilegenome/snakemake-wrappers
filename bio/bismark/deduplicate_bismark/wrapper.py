9a13be3746fc740c106e7e31a9b70bb662cab4da
Fritjof <fritjoflammers@gmail.com>
2023-08-02 17:16:02 +0200

    write custom dedup wrapper

--- bio/bismark/deduplicate_bismark/wrapper.py
+++ bio/bismark/deduplicate_bismark/wrapper.py
@@ -7,7 +7,11 @@ __email__ = "roman.chernyatchik@jetbrains.com"
 __license__ = "MIT"
 
 import os
+import shutil
+
+from pathlib import Path
 from snakemake.shell import shell
+from tempfile import TemporaryDirectory
 
 bam_path = snakemake.output.get("bam", None)
 report_path = snakemake.output.get("report", None)
@@ -16,43 +20,43 @@ if not bam_path or not report_path:
         "bismark/deduplicate_bismark: Please specify both 'bam=..' and 'report=..' paths in output section"
     )
 
-output_dir = os.path.dirname(bam_path)
-if output_dir != os.path.dirname(report_path):
-    raise ValueError(
-        "bismark/deduplicate_bismark: BAM and Report files expected to have the same parent directory"
-        " but was {} and {}".format(bam_path, report_path)
-    )
-
-arg_output_dir = "--output_dir '{}'".format(output_dir) if output_dir else ""
-arg_multiple = "--multiple" if len(snakemake.input) > 1 else ""
-
 params_extra = snakemake.params.get("extra", "")
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 log_append = snakemake.log_fmt_shell(stdout=True, stderr=True, append=True)
-shell(
-    "deduplicate_bismark {params_extra} --bam {arg_multiple}"
-    " {arg_output_dir} {snakemake.input} {log}"
-)
 
-# Move outputs into proper position.
-fst_input_filename = os.path.basename(snakemake.input[0])
-fst_input_basename = os.path.splitext(fst_input_filename)[0]
-prefix = os.path.join(output_dir, fst_input_basename)
 
-deduplicated_bam_actual_name = prefix + ".deduplicated.bam"
-if arg_multiple:
-    # bismark does it exactly like this:
-    deduplicated_bam_actual_name = deduplicated_bam_actual_name.replace(
-        "deduplicated", "multiple.deduplicated", 1
+with TemporaryDirectory() as tempdir:
+    tempdir = Path(tempdir)
+
+    input_files = []
+    for input_path in snakemake.input:
+    
+        input_bam = Path(tempdir, os.path.basename(input_path))
+        # copy input to tempdir
+        shutil.copy(os.path.basename(input_path), input_bam)
+        input_files.append(str(input_bam))
+
+    input_files = " ".join(input_files)
+
+    shell(
+        "deduplicate_bismark {params_extra} --bam "
+        " --output_dir {tempdir} {input_files} {log}"
     )
 
-expected_2_actual_paths = [
-    (bam_path, deduplicated_bam_actual_name),
-    (
-        report_path,
-        prefix + (".multiple" if arg_multiple else "") + ".deduplication_report.txt",
-    ),
-]
-for exp_path, actual_path in expected_2_actual_paths:
-    if exp_path and (exp_path != actual_path):
-        shell("mv {actual_path:q} {exp_path:q} {log_append}")
+    # Move outputs into proper position.
+    fst_input_filename = os.path.basename(snakemake.input[0])
+    fst_input_basename = os.path.splitext(fst_input_filename)[0]
+    prefix = os.path.join(tempdir, fst_input_basename)
+
+    deduplicated_bam_actual_name = prefix + ".deduplicated.bam"
+
+    expected_2_actual_paths = [
+        (bam_path, deduplicated_bam_actual_name),
+        (
+            report_path,
+            prefix + ".deduplication_report.txt",
+        ),
+    ]
+    for exp_path, actual_path in expected_2_actual_paths:
+        if exp_path and (exp_path != actual_path):
+            shell("mv {actual_path:q} {exp_path:q} {log_append}")
